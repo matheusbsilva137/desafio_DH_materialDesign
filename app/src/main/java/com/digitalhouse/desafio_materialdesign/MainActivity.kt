0483d3ec3a95aa6ce642305470aacd43316656de
Bruno <brunorbds50@gmail.com>
2020-10-14 12:14:00 -0300

    Testes realizados

--- app/src/main/java/com/digitalhouse/desafio_materialdesign/MainActivity.kt
+++ app/src/main/java/com/digitalhouse/desafio_materialdesign/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.digitalhouse.desafio_materialdesign
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btn_login.setOnClickListener{
+            makeSnack("Senha Incorreta", "Ok", it)
+        }
+    }
+
+
+    fun makeSnack(msg: String, msgAct: String, context: View){
+        Snackbar.make(context, msg, Snackbar.LENGTH_LONG)
+            .setAction(msgAct) {}
+            .show()
     }
 }
